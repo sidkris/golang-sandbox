8d8bb781b08ccf53e07d61d23e013b3f1bdde8a5
Siddharth Krishnan <sidkrishnan@protonmail.com>
2024-09-03 11:55:27 +0530

    Update go_variables.go

--- variables/go_variables.go
+++ variables/go_variables.go
@@ -7,4 +7,17 @@ func main() {
 	var myName = "Sid"
 	fmt.Println("Hi, my name is", myName)
 
+	var userName string = "_sid_"
+	fmt.Println("username :", userName)
+
+	var sum int = 5 + 6
+	fmt.Println("The sum is :", sum)
+
+	var (
+		firstInitial = 'S'
+		lastInitial  = 'K'
+	)
+
+	initials := fmt.Sprintf("First Initial : %c | Last Initial : %c", firstInitial, lastInitial)
+	fmt.Println(initials)
 }
