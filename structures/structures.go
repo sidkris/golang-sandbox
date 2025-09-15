997b0344347c1209a6943077f7aa844559e48439
Siddharth Krishnan <sidkrishnan@protonmail.com>
2025-09-15 08:46:28 +0530

    Update structures.go

--- structures/structures.go
+++ structures/structures.go
@@ -7,17 +7,17 @@ import "fmt"
 
 func main() {
 
-	type countries struct {
+	type Countries struct {
 		country          string
 		capitalCity      string
 		approxPopulation int64
 	}
 
-	var countryData []countries
+	var countryData []Countries
 
-	countryData = append(countryData, countries{"India", "New Delhi", 1400000000})
-	countryData = append(countryData, countries{"China", "Beijing", 1350000000})
-	countryData = append(countryData, countries{"USA", "Washington D.C", 350000000})
+	countryData = append(countryData, Countries{"India", "New Delhi", 1400000000})
+	countryData = append(countryData, Countries{"China", "Beijing", 1350000000})
+	countryData = append(countryData, Countries{"USA", "Washington D.C", 350000000})
 
 	for _, country := range countryData {
 		fmt.Printf("Country: %s, Capital: %s, Population: %d\n", country.country, country.capitalCity, country.approxPopulation)
